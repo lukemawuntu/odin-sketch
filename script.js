42bf0a0502660daece34ebe09af5917a2f9ddc37
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 00:38:47 +0800

    Add listener for hovering on a grid

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-getGridSize = +prompt('Choose gridsize', '16')
+let getGridSize = +prompt('Choose gridsize', '16')
 
-gridSize = getGridSize*getGridSize
+const gridSize = getGridSize*getGridSize
 for (let i = 0; i <gridSize ; i++) {
   const container=document.querySelector('.container');
-  const gridElement = document.createElement('div')
-  gridElement.classList.add('grid');
-  gridElement.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
-  container.appendChild(gridElement);
+  const gridDiv = document.createElement('div')
+  gridDiv.classList.add('grid');
+  gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
+  container.appendChild(gridDiv);
 }
+
+const gridElements = document.querySelectorAll('.grid');
+gridElements.forEach((gridElement)=>{
+  gridElement.addEventListener('mouseover',(e)=>{
+    console.log(e.target);
+  });
+});
+
+
+
