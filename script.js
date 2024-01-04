b31a4e3b40ec5043483af9ff4ae30ed058ad351b
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 03:46:22 +0800

    Add button for grid size.
    Fix issues with variables and scopes.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-let getGridSize = +prompt('Choose gridsize', '16')
+let getGridSize = 0;
+let gridSize = 0;
 
-const gridSize = getGridSize*getGridSize
-for (let i = 0; i <gridSize ; i++) {
-  const container=document.querySelector('.container');
-  const gridDiv = document.createElement('div')
-  gridDiv.classList.add('grid');
-  gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
-  container.appendChild(gridDiv);
+function changeSize(){
+  getGridSize=+prompt("input grid size","16");
+  gridSize = getGridSize*getGridSize;
+  createGrid();
 }
 
-const gridElements = document.querySelectorAll('.grid');
-gridElements.forEach((gridElement)=>{
-  gridElement.addEventListener('mouseover',(e)=>{
-    gridElement.style.backgroundColor ='yellow';
-  });
-});
+const btnChangeSize = document.querySelector('#changeSize');
+btnChangeSize.addEventListener('click',changeSize);
 
+function createGrid(){
+  const container=document.querySelector('.container');
+  for (let i = 0; i <gridSize ; i++) {
+    const gridDiv = document.createElement('div')
+    gridDiv.classList.add('grid');
+    gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
+    container.appendChild(gridDiv);
+  }
+
+  const gridElements = document.querySelectorAll('.grid');
+  gridElements.forEach((gridElement) => {
+    gridElement.addEventListener('mouseover', (e) => {
+      gridElement.style.backgroundColor = 'yellow';
+    });
+  });
+}
 
 
