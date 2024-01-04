bc352c2a8ff698edc2a32664902eba5494fe7910
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 03:54:10 +0800

    Make previous features into functions

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 let getGridSize = 0;
 let gridSize = 0;
 
-function changeSize(){
-  getGridSize=+prompt("input grid size","16");
-  gridSize = getGridSize*getGridSize;
-  createGrid();
-}
-
-const btnChangeSize = document.querySelector('#changeSize');
-btnChangeSize.addEventListener('click',changeSize);
-
 function createGrid(){
-  const container=document.querySelector('.container');
   for (let i = 0; i <gridSize ; i++) {
+    const container=document.querySelector('.container');
     const gridDiv = document.createElement('div')
     gridDiv.classList.add('grid');
     gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
     container.appendChild(gridDiv);
   }
+}
 
+function colorGrid(){
   const gridElements = document.querySelectorAll('.grid');
   gridElements.forEach((gridElement) => {
     gridElement.addEventListener('mouseover', (e) => {
       gridElement.style.backgroundColor = 'yellow';
     });
   });
+}
+
+function changeSize(){
+  getGridSize=+prompt("input grid size","16");
+  gridSize = getGridSize*getGridSize;
+  startSketch();
+}
+
+const btnChangeSize = document.querySelector('#changeSize');
+btnChangeSize.addEventListener('click',changeSize);
+
+function startSketch(){
+  createGrid();
+  colorGrid();
 }
 
 
