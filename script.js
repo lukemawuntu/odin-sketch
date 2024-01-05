9cfa14539b8e4f73c5a5bd5bc41f3c772d2d0817
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 15:26:15 +0800

    Fix bug rainbow/black not working

--- script.js
+++ script.js
@@ -1,49 +1,107 @@
 
-const changeSize = document.querySelector('#changeSize')
-changeSize.addEventListener('click',()=>{
-  const container=document.querySelector('.container');
-  container.innerHTML='';
-  for (let i = 0; i <gridSize ; i++) {
-    const gridDiv = document.createElement('div')
+// const changeSize = document.querySelector('#changeSize')
+// changeSize.addEventListener('click',start)
+
+// function changeSizeFunc(){
+//   const container=document.querySelector('.container');
+//   container.innerHTML = '';
+
+//   let getGridSize = +prompt('Choose gridsize', '16')
+
+//   const gridSize = getGridSize*getGridSize
+//   for (let i = 0; i <gridSize ; i++) {
+//     const gridDiv = document.createElement('div')
+//     gridDiv.classList.add('grid');
+//     gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
+//     container.appendChild(gridDiv);
+//   };
+// }
+
+// function start(){
+//   changeSizeFunc();
+//   toggleBlack();
+// }
+
+// const gridElements = document.querySelectorAll('.grid');
+// const rainbow = document.querySelector('#rainbow')
+// const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+// function randomInteger(){
+//   return Math.floor(Math.random()*7);
+// }
+
+// rainbow.addEventListener('click',()=>{
+//   gridElements.forEach((gridElement)=>{
+//     gridElement.addEventListener('mouseover',()=>{  
+//       gridElement.style.backgroundColor =`${rainbowColors[randomInteger()]}`;
+//     });
+//   });
+// });
+
+// function toggleBlack(){
+// const black = document.querySelector('#black')
+// black.addEventListener('click',()=>{
+//   gridElements.forEach((gridElement)=>{
+//     gridElement.addEventListener('mouseover',()=>{
+//       gridElement.style.backgroundColor ='black';
+//     });
+//   });
+// });
+// }
+
+const changeSize = document.querySelector('#changeSize');
+const black = document.querySelector('#black');
+const rainbow = document.querySelector('#rainbow');
+
+changeSize.addEventListener('click', start);
+black.addEventListener('click', toggleBlack);
+rainbow.addEventListener('click', toggleRainbow);
+
+function changeSizeFunc() {
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  let getGridSize = +prompt('Choose grid size', '16');
+
+  const gridSize = getGridSize * getGridSize;
+  for (let i = 0; i < gridSize; i++) {
+    const gridDiv = document.createElement('div');
     gridDiv.classList.add('grid');
-    gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
+    gridDiv.style.cssText = `height: ${400 / getGridSize}px; width: ${400 / getGridSize}px`;
     container.appendChild(gridDiv);
-  };
-});
+  }
 
-let getGridSize = +prompt('Choose gridsize', '16')
+  // Return the grid elements to be used in other functions
+  return document.querySelectorAll('.grid');
+}
 
-const gridSize = getGridSize*getGridSize
-for (let i = 0; i <gridSize ; i++) {
-  const container=document.querySelector('.container');
-  const gridDiv = document.createElement('div')
-  gridDiv.classList.add('grid');
-  gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
-  container.appendChild(gridDiv);
-};
+function start() {
+  const gridElements = changeSizeFunc();
+  toggleBlack(gridElements);
+  toggleRainbow(gridElements);
+}
 
-const gridElements = document.querySelectorAll('.grid');
-const rainbow = document.querySelector('#rainbow')
-const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+function toggleBlack(elements) {
+  black.addEventListener('click', () => {
+    elements.forEach((gridElement) => {
+      gridElement.addEventListener('mouseover', () => {
+        gridElement.style.backgroundColor = 'black';
+      });
+    });
+  });
+}
+
+
 function randomInteger(){
   return Math.floor(Math.random()*7);
 }
 
-rainbow.addEventListener('click',()=>{
-  gridElements.forEach((gridElement)=>{
-    gridElement.addEventListener('mouseover',()=>{  
-      gridElement.style.backgroundColor =`${rainbowColors[randomInteger()]}`;
+function toggleRainbow(elements) {
+  const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  rainbow.addEventListener('click', () => {
+    elements.forEach((gridElement) => {
+      gridElement.addEventListener('mouseover', () => {
+        gridElement.style.backgroundColor = `${rainbowColors[randomInteger()]}`;
+      });
     });
   });
-});
-
-const black = document.querySelector('#black')
-black.addEventListener('click',()=>{
-  gridElements.forEach((gridElement)=>{
-    gridElement.addEventListener('mouseover',()=>{
-      gridElement.style.backgroundColor ='black';
-    });
-  });
-});
-
-
+}
