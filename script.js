7bb5f51214445f2246d06d7c25ea13013a4bbbd1
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 12:25:13 +0800

    Fix rainbow and black button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let getGridSize = 0;
 let gridSize = 0;
-let isRainbow = false;
-let isBlack = false;
+let isRainbow = true;
+let isBlack = true;
 
 function createGrid(){
   const container=document.querySelector('.container');
@@ -18,51 +18,36 @@ function randomInteger(){
   return Math.floor(Math.random(0,7)*10)
 }
 
-function toggleRainbow(){
-  const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-  return rainbow[randomInteger()]
-}
-
-function toggleBlack(){
-  return 'black';
-}
-
-const rainbow = document.querySelector("#rainbow")
-rainbow.addEventListener('click',()=>{
-  isRainbow = true;
-})
-
-const black = document.querySelector("#black")
-black.addEventListener('click',()=>{
-  isBlack = true;
-})
-
-function colorGrid(){
-  const gridElements = document.querySelectorAll('.grid');
-  gridElements.forEach((gridElement) => {
-    gridElement.addEventListener('mouseover', () => {
-      if (isRainbow==true) {
-        gridElement.style.backgroundColor = `${toggleRainbow()}`;
-      };
-      if (isBlack==true) {
-        gridElement.style.backgroundColor = `${toggleBlack()}`;
-      };
-    });
-  });
-}
-
 function changeSize(){
   getGridSize=+prompt("input grid size","16");
   gridSize = getGridSize*getGridSize;
-  startSketch();
 }
 
 const btnChangeSize = document.querySelector('#changeSize');
 btnChangeSize.addEventListener('click',changeSize);
 
-function startSketch(){
+const rainbow = document.querySelector("#rainbow");
+rainbow.addEventListener('click', ()=>{
+  changeSize();
   createGrid();
-  colorGrid();
-}
+  const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  const gridElements = document.querySelectorAll('.grid');
+      gridElements.forEach((gridElement) => {
+        gridElement.addEventListener('mouseover', () => {
+          gridElement.style.backgroundColor = `${rainbowColors[randomInteger()]}`;
+        });
+  });
+});
 
+const black = document.querySelector("#black");
+black.addEventListener('click', ()=>{
+  changeSize();
+  createGrid();
+  const gridElements = document.querySelectorAll('.grid');
+  gridElements.forEach((gridElement) => {
+    gridElement.addEventListener('mouseover', () => {
+      gridElement.style.backgroundColor = 'black';
+    });
+  });
+});
 
