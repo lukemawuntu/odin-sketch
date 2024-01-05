084a49153390f91402fea33b8ce4bd97c01e5729
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-01-05 14:43:53 +0800

    Revert back to previous code.
    Clicking the button does not trigger new grid prompt.

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-let getGridSize = 0;
-let gridSize = 0;
-let isRainbow = true;
-let isBlack = true;
+let getGridSize = +prompt('Choose gridsize', '16')
 
-function createGrid(){
+const gridSize = getGridSize*getGridSize
+for (let i = 0; i <gridSize ; i++) {
   const container=document.querySelector('.container');
-  container.innerHTML ='';
-  for (let i = 0; i <gridSize ; i++) {
-    const gridDiv = document.createElement('div')
-    gridDiv.classList.add('grid');
-    gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
-    container.appendChild(gridDiv);
-  }
-}
+  const gridDiv = document.createElement('div')
+  gridDiv.classList.add('grid');
+  gridDiv.style.cssText=`height: ${400/getGridSize}px; width: ${400/getGridSize}px`;
+  container.appendChild(gridDiv);
+};
 
+const gridElements = document.querySelectorAll('.grid');
+const rainbow = document.querySelector('#rainbow')
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 function randomInteger(){
-  return Math.floor(Math.random(0,7)*10)
+  return Math.floor(Math.random()*7);
 }
 
-function changeSize(){
-  getGridSize=+prompt("input grid size","16");
-  gridSize = getGridSize*getGridSize;
-}
-
-const btnChangeSize = document.querySelector('#changeSize');
-btnChangeSize.addEventListener('click',changeSize);
-
-const rainbow = document.querySelector("#rainbow");
-rainbow.addEventListener('click', ()=>{
-  changeSize();
-  createGrid();
-  const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-  const gridElements = document.querySelectorAll('.grid');
-      gridElements.forEach((gridElement) => {
-        gridElement.addEventListener('mouseover', () => {
-          gridElement.style.backgroundColor = `${rainbowColors[randomInteger()]}`;
-        });
-  });
-});
-
-const black = document.querySelector("#black");
-black.addEventListener('click', ()=>{
-  changeSize();
-  createGrid();
-  const gridElements = document.querySelectorAll('.grid');
-  gridElements.forEach((gridElement) => {
-    gridElement.addEventListener('mouseover', () => {
-      gridElement.style.backgroundColor = 'black';
+rainbow.addEventListener('click',()=>{
+  gridElements.forEach((gridElement)=>{
+    gridElement.addEventListener('mouseover',()=>{  
+      gridElement.style.backgroundColor =`${rainbowColors[randomInteger()]}`;
     });
   });
 });
+
+const black = document.querySelector('#black')
+black.addEventListener('click',()=>{
+  gridElements.forEach((gridElement)=>{
+    gridElement.addEventListener('mouseover',()=>{
+      gridElement.style.backgroundColor ='black';
+    });
+  });
+});
+
 
